c34261c1f9cda4327154aba240845935a930938e
rabiori <rabiori88@jobkroea.co.kr>
2022-02-05 12:13:27 +0900

    js: toggle button

--- main.js
+++ main.js
@@ -18,6 +18,8 @@ document.addEventListener('scroll', ()=> {
     }
 });
 
+
+
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -28,8 +30,16 @@ navbarMenu.addEventListener('click', (event) => {
         return;
     }
 
+    navbarMenu.classList.remove('open');
+
     scrollIntoView(link);
 });
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+})
 
 // Handle click on "contact me" button on home
 
